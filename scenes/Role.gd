bade62208e689137b571bf0cec740b7ea60e6bf9
tzz <283853646@qq.com>
2020-04-03 22:47:39 +0800

    update:face_direct

--- scenes/Role.gd
+++ scenes/Role.gd
@@ -6,7 +6,26 @@ extends Pseudo3D
 # var b = "text"
 
 
+var face_direct_right =true
+signal face_direct_change
+var signal_face_direct_change="face_direct_change"
 
+func handle_face_direct_change(event:InputEvent):
+	var change = false
+	if event.is_action_pressed("ui_right"):
+		if !self.face_direct_right :
+			change =true
+	if event.is_action_pressed("ui_left"):
+		if self.face_direct_right:
+			change =true
+	if change :
+		self.face_direct_right = !self.face_direct_right
+		self.get_node("Sprite").flip_h = !self.face_direct_right
+
+
+
+func _unhandled_input(event):
+	self.handle_face_direct_change(event)
 
 func _ready():
 	pass
